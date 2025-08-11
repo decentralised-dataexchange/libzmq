46dd1926c31abc1e651e69ed0d927c4e265eedcd
Joseph Milan K.A <josmilan1103@gmail.com>
2025-08-11 09:49:09 +0530

    Initial commit: Add Swift Package Manager support for libzmq

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "libzmq",
+    platforms: [
+        .iOS(.v12),
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "libzmq",
+            targets: ["libzmq"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/decentralised-dataexchange/libsodium", from: "2025.8.1")
+    ],
+    targets: [
+        .target(
+            name: "libzmq",
+            dependencies: [
+                .product(name: "libsodium", package: "libsodium")
+            ],
+            path: "Sources/libzmq",
+            exclude: [
+                    "tweetnacl.c",
+                    "tweetnacl.h" // whatever files implement tweetnacl
+                ],
+            publicHeadersPath: "include",
+            cSettings: [
+                .headerSearchPath("include")
+            ],
+            cxxSettings: [
+                .headerSearchPath("include"),
+                .unsafeFlags(["-std=c++17"])
+            ]
+        )
+    ]
+)
